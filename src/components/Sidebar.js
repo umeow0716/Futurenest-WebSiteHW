88f6db63ef7f97179306638b1dc3d148e468c512
123 123 <jj30462281@gmail.com>
2023-05-13 02:34:25 +0800

    Sidebar Titles Complete

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,9 +1,13 @@
+import React, { Component } from 'react';
+
 import styled from 'styled-components'
 import icon from './icon.svg'
 import IconList from './IconList.js'
 
 import right from './right.js'
 import down from './down.js'
+
+import Buttons from './Buttons.js'
 
 const Base = styled.div`
     background: #FFFFFF;
@@ -27,8 +31,18 @@ const ItemList = styled.div`
     }
 `;
 
-const Item = (props) => {
-    const Object = styled.div`
+class Item extends Component{
+    constructor(props) {
+        super(props);
+        this.props = props;
+
+        this.state = {
+            data: "right",
+            jsx: <Buttons.right />
+        }
+    }
+
+    Object = styled.div`
         display: flex;
         
         border-radius: 10px;
@@ -58,23 +72,41 @@ const Item = (props) => {
         }
     `;
 
-    const TextBox = styled.b`
+    TextBox = styled.b`
         font-family: Inter;
         font-size: 19px;
         padding: 1vmin;
     `;
 
-    const LastIcon = styled.div`
+    LastIcon = styled.div`
         display: flex;
         margin-left: auto;
     `;
-    return (
-        <Object>
-            <props.icon alt="icon"/>
-            <TextBox>{props.text}</TextBox>
-            <LastIcon>{props.LastIcon ? <props.LastIcon /> : ""}</LastIcon>
-        </Object>
-    )
+    
+    change = () => {
+        if (this.state.data === "right") {
+            this.setState({
+                data: "down",
+                jsx: <Buttons.down />
+            });
+        }
+        else {
+            this.setState({
+                data: "right",
+                jsx: <Buttons.right />
+            });
+        }
+    }
+
+    render() {
+        return (
+            <this.Object onClick={this.change}>
+                <this.props.icon alt="icon"/>
+                <this.TextBox>{this.props.text}</this.TextBox>
+                {this.props.LastIcon ? (<this.LastIcon>{this.state.jsx}</this.LastIcon>) : ""}
+            </this.Object>
+        )
+    }
 }
 
 
@@ -85,11 +117,11 @@ function Sidebar() {
             <img src={icon} alt="logo" />
             <ItemList>
                 <Item icon={IconList.Overview} text="Overview"/>
-                <Item icon={IconList.Finance} LastIcon={right} text="Finance" alt="right"/>
-                <Item icon={IconList.Sales} LastIcon={right} text="Sales"/>
-                <Item icon={IconList.HR} LastIcon={right} text="HR"/>
-                <Item icon={IconList.Invoicing} LastIcon={right} text="Invoicing"/>
-                <Item icon={IconList.Genuine_AI} LastIcon={right} text="Genuie AI"/>
+                <Item icon={IconList.Finance} LastIcon="1" text="Finance" alt="right"></Item>
+                <Item icon={IconList.Sales} LastIcon="1" text="Sales"></Item>
+                <Item icon={IconList.HR} LastIcon="1" text="HR"></Item>
+                <Item icon={IconList.Invoicing} LastIcon="1" text="Invoicing"></Item>
+                <Item icon={IconList.Genuine_AI} LastIcon="1" text="Genuie AI"></Item>
             </ItemList>
         </Base>
     )
