2c2d87ab42ef6fe712f3d95b8390a26b6a6b3ad8
123 123 <jj30462281@gmail.com>
2023-05-16 14:36:24 +0800

    Navbar update 20230516-2

--- src/components/CenterColumn/CenterColumn.js
+++ src/components/CenterColumn/CenterColumn.js
@@ -1,7 +1,26 @@
 import './CenterColumn';
+import styled from 'styled-components'
+
+const test = styled.div`
+    postion: relative;
+`
+
 function CenterColumn() {
     return (
-        <h1>A Test Content</h1>
+        <div>
+            <h1>A Test Content</h1>
+            <h1>A Test Content</h1>
+            <h1>A Test Content</h1>
+            <h1>A Test Content</h1>
+            <h1>A Test Content</h1>
+            <h1>A Test Content</h1>
+            <h1>A Test Content</h1>
+            <h1>A Test Content</h1>
+            <h1>A Test Content</h1>
+            <h1>A Test Content</h1>
+            <h1>A Test Content</h1>
+            <h1>A Test Content</h1>
+        </div>
     )
 }
 
