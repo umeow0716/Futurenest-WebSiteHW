30aa631816c32c66a5b14cfefad837b8ca353f88
123 123 <jj30462281@gmail.com>
2023-05-17 17:06:07 +0800

    CenterColumn Overview 方塊完成

--- src/components/CenterColumn/CenterColumn.js
+++ src/components/CenterColumn/CenterColumn.js
@@ -1,4 +1,4 @@
-import './CenterColumn';
+import './CenterColumn.css';
 import styled from 'styled-components'
 
 const test = styled.div`
@@ -7,19 +7,69 @@ const test = styled.div`
 
 function CenterColumn() {
     return (
-        <div>
-            <h1>A Test Content</h1>
-            <h1>A Test Content</h1>
-            <h1>A Test Content</h1>
-            <h1>A Test Content</h1>
-            <h1>A Test Content</h1>
-            <h1>A Test Content</h1>
-            <h1>A Test Content</h1>
-            <h1>A Test Content</h1>
-            <h1>A Test Content</h1>
-            <h1>A Test Content</h1>
-            <h1>A Test Content</h1>
-            <h1>A Test Content</h1>
+        <div className='container'>
+            <div className='Overview'>
+                <h1> Overview</h1>
+                <h3> Finance</h3>
+                <div className='AccumulatedProfitAndLoss'>
+                    
+                </div>
+                <div className='MonthlyIncome'>
+                <img src='OV_Fi_MonthlyIncome.jpg'></img>
+                </div>
+                <div className='FinanceRow'>
+                    <div className='CurrentCashProfile'>
+
+                    </div>
+                    <div className='CurrentAccountsReceivable'>
+
+                    </div>
+                </div>
+                <div className='FinanceRow'>
+                    <div className='CashFlowThisMonth'>
+
+                    </div>
+                    <div className='CurrentAccountsPayable'>
+
+                    </div>
+
+                </div>
+                <div className='FinanceRow'>
+                    <div className='TotalPettyCashOutgoing'>
+
+                    </div>
+                    <div className='RatioOfOperatingExpenses'>
+
+                    </div>
+
+                </div>
+                <div className='PettyCashInAndOutStatus'>
+
+                </div>
+                <h3> Sales</h3>
+                <div className='AchievementRate'>
+
+                </div>
+                <div className='AchievementRateOfBusinessBody'>
+
+                </div>
+                <div className='SalesRow'>
+                    <div className='ProductSalesRanking'>
+
+                    </div>
+                    <div className='CustomerSalesRanking'>
+
+                    </div>
+                    <div className='SalesmanSalesRanking'>
+
+                    </div>
+                </div>
+            </div>
+            <div className='buttom'>
+                <h5>Copyright @ 2020-2023 Globex Corporation</h5>
+            </div>
+
+
         </div>
     )
 }
