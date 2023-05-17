88144c6309779fa3fcf1dcec49623b36882de25d
123 123 <jj30462281@gmail.com>
2023-05-18 02:13:19 +0800

    SalesAnalysis update

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css'
 import styled from 'styled-components'
 import Sidebar from './components/Sidebar.js'
 import CenterColumn from './components/CenterColumn/CenterColumn'
+import SalesAnalysis from './components/SalesAnalysis/SalesAnalysis'
 import Notification from './Notification.svg'
 import Cloud from './Cloud.svg'
 
@@ -38,7 +39,7 @@ function App() {
             </div>
         
             <div className='context'>
-                <CenterColumn />
+                <SalesAnalysis />
             </div>
         </div>  
       </Main>
